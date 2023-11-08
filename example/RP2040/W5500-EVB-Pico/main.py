5db499a238197e14e06623f03a90bce5c383db94
Marcos José Rangel <marcos.j.r.g.j@gmail.com>
2023-11-08 15:15:22 -0300

    first working version

--- example/RP2040/W5500-EVB-Pico/main.py
+++ example/RP2040/W5500-EVB-Pico/main.py
@@ -9,17 +9,21 @@ nic = network.WIZNET5K()
 nic.active(True)
 nic.ifconfig("dhcp")
 
-# Define your Fichar.io credentials
-username = "<USERNAME>"
-passwd = "<PASSWORD>"
-deviceID = "<DEVICEID>"
+MOCK_BTN_STATE = 0
+def mock_btn():
+    global MOCK_BTN_STATE
+    if MOCK_BTN_STATE == 1: MOCK_BTN_STATE = 0
+    else: MOCK_BTN_STATE = 1
+    return MOCK_BTN_STATE
+
+from credentials import *
 
 fichario = Fichario(
     uniqueId = ubinascii.hexlify(unique_id()).decode(),
     username = username,
     passwd   = passwd,
     deviceID = deviceID,
-    timerID  = 1,
+    timerID  = -1,
     server   = "br1.data.fichar.io",
     KeepOn   = True,
     ssl      = True,
@@ -28,3 +32,16 @@ fichario = Fichario(
 
 fichario.TIMESTAMP_METHOD = time.time
 
+fichario.add_new_payload(PayloadPkgMaker(name="btn_state",
+    callback = mock_btn,
+    unit = "unt",
+    min = 0,
+    max = 1,
+    trg = 0
+))
+
+tim2 = Timer(-1)
+def main():
+    print("starting main function...")
+    fichario.just_do_it() ## First run
+    tim2.init(period=60000, mode=Timer.PERIODIC, callback=fichario.just_do_it) ## calls "just do it" periodcaly
